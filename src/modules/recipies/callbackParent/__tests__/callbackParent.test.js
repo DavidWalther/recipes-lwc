daa9af44e1a543488298a469b50aabdbd609a40f
David Walther <walthdav@gmail.com>
2020-04-18 20:34:56 +0200

    pass callback from parent to child

--- src/modules/recipies/callbackParent/__tests__/callbackParent.test.js
+++ src/modules/recipies/callbackParent/__tests__/callbackParent.test.js
@@ -10,9 +10,36 @@ describe('', () => {
     });
 
     test('include element into DOM', () => {
+        const handler = jest.fn();
+
+        /**
+         * Given
+         * the parent cmp is added to DOM
+         */
         const element = createElement('recipe-callback-parent', {
             is: CallbackParent
         });
+        element.addEventListener('date', handler);
         document.body.appendChild(element);
+
+        /**
+         * When
+         * it's button is clicked
+         */
+        const button = element.shadowRoot.querySelector('input[type=button]');
+        button.dispatchEvent(new CustomEvent('click'));
+
+        // Wait for events to be processed
+        return Promise.resolve().then(() => {
+            /**
+             * Then
+             * A 'date'-event is fired containing the value passed by the child component
+             */
+
+            expect(handler).toBeCalled();
+            expect(handler.mock.calls[0][0]).toBeDefined();
+            expect(handler.mock.calls[0][0].detail).toBeDefined();
+            expect(handler.mock.calls[0][0].detail.timestamp).toBeDefined();
+        });
     });
 });
--- src/modules/recipies/callbackParent/callbackParent.js
+++ src/modules/recipies/callbackParent/callbackParent.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
 
-export default class CallbackParent extends LightningElement {}
+export default class CallbackParent extends LightningElement {
+    onClickButtonChildCallback() {
+        const value = this.callMethodAndReturnDate();
+        this.fireEvent(value);
+    }
+
+    callMethodAndReturnDate() {
+        const callbackChild = this.template.querySelector(
+            'recipies-callback-child'
+        );
+        let returnValue;
+        callbackChild.returnDate((value) => {
+            returnValue = value;
+        });
+        return returnValue;
+    }
+
+    fireEvent(value) {
+        const evt = new CustomEvent('date', { detail: { timestamp: value } });
+        this.dispatchEvent(evt);
+    }
+}
